fdd372bf2f40ee77d4946d8d51607f3db5eeeffb
Dominic Letz <dominic@diode.io>
2020-08-17 19:16:06 +0200

    Updated ledger-wallet-libs

--- dapp/copy.sh
+++ dapp/copy.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+export NODE_ENV=production
 npm run build-libs-web
 rm -rf /home/dominicletz/projects/diode/website/multisig/
 cp -R src/ /home/dominicletz/projects/diode/website/multisig
